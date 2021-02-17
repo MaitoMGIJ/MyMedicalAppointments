d743669d85a745549b23a795e655b559da7abbf5
Mauricio Avila <mao979797@gmail.com>
2021-02-16 19:55:05 -0500

    Variables y metodos estaticos

--- src/Doctor.java
+++ src/Doctor.java
@@ -1,11 +1,12 @@
 public class Doctor {
 
-    int id;
+    static int id = 0;
     String name;
     String speciality;
 
     Doctor(){
         System.out.println("Construyendo un objeto Doctor");
+        id++;
     }
 
     Doctor(String name){
@@ -14,5 +15,9 @@ public class Doctor {
 
     public void showName(){
         System.out.println(name);
+    }
+
+    public void showId(){
+        System.out.println("ID Doctor: "+id);
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,22 @@
+import static ui.UIMenu.*;
+
 public class Main {
     public static void main(String[] args) {
-        Doctor myDoctor = new Doctor("Mauricio Avila");
+        Doctor myDoctor = new Doctor();
         myDoctor.name = "Anahí Salgado";
         myDoctor.showName();
+        myDoctor.showId();
+        System.out.println(Doctor.id);
+
+        Doctor.id++;
+
+        Doctor myDoctorMao = new Doctor();
+        myDoctorMao.name = "Mauricio Avila";
+        myDoctorMao.showName();
+        myDoctorMao.showId();
+        System.out.println(Doctor.id);
+
+        showMenu();
+
     }
 }
